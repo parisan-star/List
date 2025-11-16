a8ccf0fbc0283757371d0e333dd2ccc56a81cef7
Parisan <parisanrasoulian@gmail.com>
2025-11-16 22:03:48 +0330

    Add files via upload

---
+++ FrmPerson.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Information
+{
+    public partial class FrmPerson : Form
+    {
+        public FrmPerson()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            var Peaple = new Person();
+
+            Peaple.NationalCode = int.Parse(txtNationalCode.Text);
+            Peaple.Name = txtName.Text;
+            Peaple.LastName = txtLastName.Text;
+            Peaple.Gender = txtGender.Text;
+            var frmMain = Application.OpenForms["FrmMain"] as FrmMain;
+            frmMain.peaple.Add(Peaple);
+            this.Close();
+            
+        }
+     }
+}
